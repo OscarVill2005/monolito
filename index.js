605ad1084736c03f38fbabd959657b40c6877f0e
OscarEmpirico <theoscardream@outlook.es>
2025-09-26 15:03:20 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const app = express()
+const port = 3000
+
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.render('index', {title: "Titulo", name:"Nombre"});
+})
+
+isAuth = (req, res, next) => {
+  if (req.cookies && req.cookies.user){
+    return next();
+  }
+  res.redirect('login');
+}
+
+//gestion vista
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+//gestion de los parametros post
+app.post('/login', (req, res) => {
+  const {user, password} = req.body
+  console.log(req.body)
+
+  if(req.body.user === "oscar" && req.body.password === "12345"){
+    res.cookie("user", user) // options - js no secure si
+    res.redirect("home")
+  }else{
+    res.status(401).redirect("login")
+  }
+})
+
+app.get('/home', isAuth, (req, res) => {
+  res.render('home')
+})
+
+app.get('/logout', isAuth, (req, res) => {
+  res.clearCookie('user')
+  res.redirect('login')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
+
